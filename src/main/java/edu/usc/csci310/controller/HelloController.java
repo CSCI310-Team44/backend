6bf28dcdee163dfdbda3546f716f6711a093f89d
zheweixu <zheweixu@usc.edu>
2022-03-10 19:03:32 -0800

    init

---
+++ src/main/java/edu/usc/csci310/controller/HelloController.java
@@ -0,0 +1,13 @@
+package edu.usc.csci310.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
