2bd271757156dddfbc2a87906ad22eb0b01f76a8
Gary Mao <yunhanm@usc.edu>
2022-04-13 23:55:15 -0700

    update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,23 @@ INSERT INTO users (userid, username, password, fname, lname, photopath)
 VALUES (1000000002, 'ttrojan', 'password', 'tommy', 'trojan', 'C:\\users');
 INSERT INTO users (userid, username, password, fname, lname, photopath)
 VALUES (1000000003, 'zx99', 'password', 'billy', 'bruin', 'C:\\users');
+
+INSERT INTO booking (bookingid, userid, reccenterid, timeslot, iswaitlist)
+VALUES (1, 1000000001, 1, '1997-12-31 10:00:00', false);
+INSERT INTO booking (bookingid, userid, reccenterid, timeslot, iswaitlist)
+VALUES (2, 1000000002, 1, '1997-12-31 10:00:00', true);
+INSERT INTO booking (bookingid, userid, reccenterid, timeslot, iswaitlist)
+VALUES (3, 1000000002, 1, '1998-01-01 10:00:00', false);
+INSERT INTO booking (bookingid, userid, reccenterid, timeslot, iswaitlist)
+VALUES (4, 1000000002, 2, '1998-01-02 10:00:00', false);
+INSERT INTO booking (bookingid, userid, reccenterid, timeslot, iswaitlist)
+VALUES (5, 1000000001, 2, '2022-3-30 14:00:00', false);
+INSERT INTO booking (bookingid, userid, reccenterid, timeslot, iswaitlist)
+VALUES (6, 1000000002, 2, '2022-3-30 14:00:00', true);
+
+INSERT INTO vacancy (vacancyid, reccenterid, timeslot, numvacant)
+VALUES (1, 1, '1997-12-31 10:00:00', 0);
+INSERT INTO vacancy (vacancyid, reccenterid, timeslot, numvacant)
+VALUES (2, 1, '1998-01-01 10:00:00', 1);
+INSERT INTO vacancy (vacancyid, reccenterid, timeslot, numvacant)
+VALUES (3, 2, '1998-01-02 10:00:00', 3);
